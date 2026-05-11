548cb0efec3524dc4bf8563746dde4b22b1d8127
vimo-ae <veddertimothy@gmail.com>
2026-05-11 18:01:52 +0700

    edit env example

--- Cuanify/routes/web.php
+++ Cuanify/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome-template');
+    return view('welcome');
 });
 
 Route::get('/ui-login', function () {
