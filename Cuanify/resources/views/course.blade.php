0396a3ef1bd17c5b7a6ab2a7db39f16b4ad78ba8
vimo-ae <veddertimothy@gmail.com>
2026-05-12 19:32:37 +0700

    tambah sikit template halaman courses

--- Cuanify/resources/views/course.blade.php
+++ Cuanify/resources/views/course.blade.php
@@ -5,11 +5,29 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="pt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Selamat bekerja...") }}
+                    {{ __("Lesson 1...") }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pt-5">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Lesson 2...") }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pt-5">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Lesson 3...") }}
                 </div>
             </div>
         </div>
