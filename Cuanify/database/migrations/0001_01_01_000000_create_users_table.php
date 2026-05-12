b40a4e94aebbed009136df167f0e978dbafe743b
41vin-cyber <sigalinggingalvin021@gmail.com>
2026-05-12 10:21:38 +0700

    Menambahkan migration tabel

--- Cuanify/database/migrations/0001_01_01_000000_create_users_table.php
+++ Cuanify/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,28 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('user_id');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->enum('role', ['admin', 'instructor', 'student'])->default('student');
+            $table->boolean('is_verified')->default(false);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('last_login')->nullable();
+            $table->enum('status', ['active', 'inactive', 'banned'])->default('active');
         });
     }
 
