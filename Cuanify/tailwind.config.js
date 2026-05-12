5f6f9dbdb80d6613a09ac213fa78f126cda77a26
vimo-ae <veddertimothy@gmail.com>
2026-05-12 12:16:14 +0700

    add courses branch

--- Cuanify/tailwind.config.js
+++ Cuanify/tailwind.config.js
@@ -3,7 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'false', // ganti ke 'false' untuk light mode, ganti ke 'media' untuk mode sesuai device
+    darkMode: 'light', // ganti ke 'false' untuk light mode, ganti ke 'media' untuk mode sesuai device
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
