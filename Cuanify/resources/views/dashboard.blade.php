bdda8b4365a43427e557d49eca8a60c43e42ae6f
vimo-ae <veddertimothy@gmail.com>
2026-05-12 19:29:42 +0700

    tambah sikit template halaman courses

--- Cuanify/resources/views/dashboard.blade.php
+++ Cuanify/resources/views/dashboard.blade.php
@@ -15,14 +15,34 @@
         </div>
     </div>
 
-    <div class="py-1">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="max-w-7xl py-2 mx-auto sm:px-6 lg:px-8">
+            <div>
                 <a
                     href="{{ url('/course') }}"
                     class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                 >
-                    Test Course
+                    Test Course 1
+                </a>
+            </div>
+        </div>
+        <div class="max-w-7xl py-2 mx-auto sm:px-6 lg:px-8">
+            <div>
+                <a
+                    href="{{ url('/course') }}"
+                    class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                >
+                    Test Course 2
+                </a>
+            </div>
+        </div>
+        <div class="max-w-7xl py-2 mx-auto sm:px-6 lg:px-8">
+            <div>
+                <a
+                    href="{{ url('/course') }}"
+                    class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                >
+                    Test Course 3
                 </a>
             </div>
         </div>
