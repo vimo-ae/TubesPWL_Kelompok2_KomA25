e25b329d51b13e3243ae68977169703dc7b84e88
aliyahbrieksalem-cell <aliyahbrieksalem@gmail.com>
2026-05-15 20:26:49 +0700

    update(3): fitur dashboard tambahan

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ui-login', function () {
+    return view('ui-login');
+});
+
+Route::get('/ui-register', function () {
+    return view('ui-register');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
