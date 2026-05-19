9fc4855c00d6d05a609b5ef46e5d501d915299f3
RynFrr <rianginting541@gmail.com>
2026-05-19 08:52:12 +0700

    Tambah halaman buat nampilin course secara dinamis

--- Cuanify/routes/web.php
+++ Cuanify/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/courses', [CourseController::class, 'index'])
+    ->name('courses.index');
 
 require __DIR__.'/auth.php';
