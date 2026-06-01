3abd405c8940330e77238466417665ea289e49d0
RynFrr <rianginting541@gmail.com>
2026-06-01 21:40:33 +0700

    memperbaiki tampilan show course dan tombol enroll untuk student

--- Cuanify/resources/views/layouts/sidebar.blade.php
+++ Cuanify/resources/views/layouts/sidebar.blade.php
@@ -21,10 +21,16 @@
             </a>
             
         @elseif(auth()->user()->role === 'student')
-            <a href="{{ url('/courses') }}" 
+            <a href="{{ url('/my-courses') }}" 
                class="w-full flex items-center space-x-3 py-3 px-4 rounded-lg transition-all duration-300 text-gray-600 hover-gradient font-medium">
                 <i class="fas fa-book-open text-sm"></i>
                 <span>Course Saya</span>
+            </a>
+
+            <a href="{{ route('courses.index') }}" 
+               class="w-full flex items-center space-x-3 py-3 px-4 rounded-lg transition-all duration-300 text-gray-600 hover-gradient font-medium">
+                <i class="fas fa-globe text-sm"></i>
+                <span>Semua Course</span>
             </a>
         @endif
 
