156bac2aea5f2099d8db29d9847269040b2942ea
vimo-ae <veddertimothy@gmail.com>
2026-06-11 20:57:31 +0700

    debug seeder lagi

--- Cuanify/database/seeders/ReviewSeeder.php
+++ Cuanify/database/seeders/ReviewSeeder.php
@@ -12,32 +12,6 @@ class ReviewSeeder extends Seeder
      */
     public function run(): void
     {
-        Review::create([
-            'user_id' => 1,
-            'course_id' => 1,
-            'rating' => 5,
-            'comment' => 'Course sangat mudah dipahami!',
-        ]);
-
-        Review::create([
-            'user_id' => 2,
-            'course_id' => 1,
-            'rating' => 4,
-            'comment' => 'Materi bagus dan jelas.',
-        ]);
-
-        Review::create([
-            'user_id' => 1,
-            'course_id' => 2,
-            'rating' => 5,
-            'comment' => 'Sangat membantu untuk pemula.',
-        ]);
-
-        Review::create([
-            'user_id' => 3,
-            'course_id' => 2,
-            'rating' => 3,
-            'comment' => 'Lumayan bagus tapi kurang latihan.',
-        ]);
+      
     }
 }
